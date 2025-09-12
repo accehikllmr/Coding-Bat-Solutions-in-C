b679c398de6ba429defc1c92b685a152e89731cd
Michel Clark <accehikllmr@kalinux.domain>
2025-09-12 10:24:10 -0400

    progress, but still stuck with getting data from recursive calls, up the stack

--- Recursion-1/strDist.c
+++ Recursion-1/strDist.c
@@ -12,7 +12,7 @@
 
 #define SIZE 50
 
-unsigned long strDist(char *, char *);
+unsigned long strDist(char *, char *, int);
 
 int main() {
 	
@@ -26,14 +26,17 @@ int main() {
 	fgets(inputSub, SIZE, stdin);
 	*(inputSub + strlen(inputSub) - 1) = 0;
 
-	printf("largest substring which start and ends with %s in %s is of length %lu\n", 
-			inputStr, inputSub, strDist(inputStr, inputSub));
+	printf("largest substring which starts and ends with %s in %s is of length %lu\n", 
+			inputStr, inputSub, strDist(inputStr, inputSub, 0));
 			
 	return 0;
 }
 
-unsigned long strDist(char *str, char *sub) {
+unsigned long strDist(char *str, char *sub, int begin) {
 	
+	int *beginPtr = str;
+	int *endPtr = str;
+
 	if (*str != 0) {
 
 		if (*str == *sub) {
@@ -42,13 +45,34 @@ unsigned long strDist(char *str, char *sub) {
 
 			if (*sub == 0) {
 
-	
-			}
+				//
+				endPtr = sub - 1;
+				// resetting pointer to beginning of substring
+				sub = sub - strlen(sub);
+				// set the starting address for the ptr, end can be calculated later
+				if (begin != 1) {
+					
+					begin = 1;
+					beginPtr = str - strlen(sub - 1);
+				}
+			
+			} else strDist(str + 1, sub, begin);
+		
+		} else {
+
+			// need to handle case when some letters matched
+
+
+			strDist(str + 1, sub, begin);
+
 		}
 	}
 
+	return endPtr - beginPtr;
+
 }
 
+// HOW TO TRACK VALUE OF BEGIN PTR ACROSS RECURSIVE CALLS, MAYBE TRACK LENGTH INSTEAD
 // not found -> 0
 // found once -> strlen(sub)
 // found once, traverse, find again, substract ptrs
