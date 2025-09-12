f33a76dc73a159c8d49fe2d72413efba237e0bdc
Michel Clark <accehikllmr@kalinux.domain>
2025-09-12 09:50:43 -0400

    completed first advanced challenge, using binary trees

--- Recursion-2/Complete/splitArray.c
+++ Recursion-2/Complete/splitArray.c
@@ -8,10 +8,11 @@
  */
 
 #include<stdio.h>
+#include<stdlib.h>
 
 int arraySum(int *, int*);
-int splitArray(int *, int *, int *, int *);
-int splitArrayHelper(int *, int *, int *, int *, int, int);
+int splitArray(int *, int *, int *, int *, int);
+int splitArrayHelper(int *, int *, int *, int *, int, int, int);
 
 int main() {
 
@@ -29,9 +30,9 @@ int main() {
 	int *rightArray = (int *)calloc(size, sizeof(int));
 	int *endInputArray = inputArray + size;
 
-	char *result = (splitArray(inputArray, endInputArray, leftArray, rightArray) != 0) ? "CAN" : "CANNOT";
+	char *result = (splitArray(inputArray, endInputArray, leftArray, rightArray, size) != 0) ? "CAN" : "CANNOT";
 
-	printf("array %s be split into two groups, such that each group has the same sums of elements");
+	printf("array %s be split into two groups, such that each group has the same sums of elements\n", result);
 
 	free(inputArray);
 	free(leftArray);
@@ -49,32 +50,36 @@ int arraySum(int *from, int *to) {
 }
 
 /* returns result of helper function */
-int splitArray(int *array, int *arrayEnd, int *left, int *right) {
+int splitArray(int *array, int *arrayEnd, int *left, int *right, int size) {
 	// passing ptr to first and last elements of array, and ptrs to pair of new arrays with insertion index
-	return splitArrayHelper(array, arrayEnd, left, right, 0, 0); 
+	return splitArrayHelper(array, arrayEnd, left, right, 0, 0, size); 
 }
 
 /* */
-int splitArrayHelper(int *from, int *to, int *left, int *right, int leftIndex, int rightIndex) {	
+int splitArrayHelper(int *from, int *to, int *left, int *right, int leftIndex, int rightIndex, int size) {
+	// stores result of given recursive call, and all other recursive calls made from this call (see below)
+	int result = 0;
 	// no need to evaluate empty array case separately, since unmodified initialized arrays will have same sum
 	// iterating over entire size, regardless of number of elements added from array (i.e. rest are 0s)
 	if (from == to) {
-		// returning value only if true, otherwise will return to line before recursive call
-		if (arraySum(left, left + size - 1) == arraySum(right, right + size - 1)) return true;
+		// increments resutlt and goes back up the stack to previous recursive call, check size to prevent invalid memory access
+		result += (size != 0) ? arraySum(left, left + size - 1) == arraySum(right, right + size - 1) : 1;
 	}
 	// pointer traversing array has not yet reached memory address located after end of array
 	else {
 		// place element at array index in left array (first option) at left index
 		*(left + leftIndex) = *from;
 		// recursive call to place following elements in array, incrementing left index since added element
-		splitArrayHelper(from + 1, to, left, right, leftIndex + 1, rightIndex);
+		result += splitArrayHelper(from + 1, to, left, right, leftIndex + 1, rightIndex, size);
 		// reset value to zero for index, since did not return true when comparing sums
 		*(left + leftIndex) = 0;
 
 		// repeat of above algorithm, but for adding elements to right array
-		*(right + rigthIndex) = *from;
-		splitArrayHelper(from + 1, to, left, right, leftIndex, rightIndex + 1);
-		*(right + rigthIndex) = 0;
+		*(right + rightIndex) = *from;
+		result += splitArrayHelper(from + 1, to, left, right, leftIndex, rightIndex + 1, size);
+		*(right + rightIndex) = 0;
 	}
+	// algorithm not efficient, will traverse entire binary tree even after finding result (i.e. result > 1)
+	return result != 0;
 }
 
